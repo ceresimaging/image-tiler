36cbbb6248fa3e8d0d1c49fa7f76734b3cadfab8
Mauricio Miranda <maurimiranda@gmail.com>
2020-07-17 17:56:22 -0300

    Refactor tree layers

--- middlewares/tree.js
+++ middlewares/tree.js
@@ -3,26 +3,45 @@ import mapnik from 'mapnik';
 // Load Mapnik datasource
 mapnik.registerDatasource(`${mapnik.settings.paths.input_plugins}/postgis.input`);
 
-const buildQuery = (field) => {
+const buildTreeQuery = (field) => {
   return `(
     SELECT
-      s.id::text spot_id,
       t.id::text tree_id,
       df.id::text AS field_id,
-      s.geometry AS geom,
+      t.geometry AS geom,
       t.plant_date,
+      t.remove_date,
       v.id::text varietal_id,
       v.name varietal
-    FROM spots s
-    LEFT JOIN spot_trees st ON st.spot_id = s.id
-    LEFT JOIN trees t ON t.id = st.tree_id
+    FROM trees t
     LEFT JOIN customers_cropvarietal v ON v.id = t.varietal_id
-    JOIN published_imagery_displayfield df ON df.source_field_id = s.field_id
+    JOIN published_imagery_displayfield df ON df.source_field_id = t.field_id
     WHERE df.id = '${field}'
+      AND t.deleted is NULL 
+      AND t.is_present = true
   ) AS trees`;
 };
 
-const buildDataSource = (field) => {
+const buildMissingQuery = (field) => {
+  return `(
+    SELECT
+      t.id::text tree_id,
+      df.id::text AS field_id,
+      t.geometry AS geom,
+      t.plant_date,
+      t.remove_date,
+      v.id::text varietal_id,
+      v.name varietal
+    FROM trees t
+    LEFT JOIN customers_cropvarietal v ON v.id = t.varietal_id
+    JOIN published_imagery_displayfield df ON df.source_field_id = t.field_id
+    WHERE df.id = '${field}'
+      AND t.deleted is NULL 
+      AND t.is_present = false
+  ) AS trees`;
+};
+
+const buildDataSource = (field, queryBuilder) => {
   return new mapnik.Datasource({
     type: 'postgis',
     host: process.env.CORE_DB_HOST,
@@ -30,7 +49,7 @@ const buildDataSource = (field) => {
     user: process.env.CORE_DB_USER,
     password: process.env.CORE_DB_PASS,
     dbname: process.env.CORE_DB_NAME,
-    table: buildQuery(field),
+    table: queryBuilder(field),
     extent_from_subquery: true,
     geometry_field: 'geom',
     srid: 4326,
@@ -43,11 +62,13 @@ export const treeLayer = (req, res, next) => {
   const { field } = req.params;
   const { map } = res.locals;
 
-  const layer = new mapnik.Layer('trees');
+  const trees = new mapnik.Layer('trees');
+  trees.datasource = buildDataSource(field, buildTreeQuery);
+  map.add_layer(trees);
 
-  layer.datasource = buildDataSource(field);
-
-  map.add_layer(layer);
+  const missing = new mapnik.Layer('missing');
+  missing.datasource = buildDataSource(field, buildMissingQuery);
+  map.add_layer(missing);
 
   next();
 };
