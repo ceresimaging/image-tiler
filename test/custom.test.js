f51feac5bc5de045e04a0acb1496bc80ff730c8a
Mauricio Miranda <maurimiranda@gmail.com>
2020-07-29 22:03:12 -0300

    Fix custom tests

--- test/custom.test.js
+++ test/custom.test.js
@@ -3,11 +3,9 @@ import { app, request, fixture, downloadCustom } from './helpers';
 const base = 'custom';
 const custom = '0e220754-e251-41c2-ab8b-0f05962ab7e9';
 
-describe('custom routes', () => {
-  beforeAll(() => {
-    return downloadCustom(custom);
-  });
+jest.mock('aws-sdk');
 
+describe('custom routes', () => {
   test('should return a vector tile', async done => {
     const res = await request.get(`/${base}/${custom}/14/2680/6344.mvt`).responseType('arraybuffer');
 
