bc8dc71cc280ffce877c7c1c1bb3ad63b13a5b29
Mauricio Miranda <maurimiranda@gmail.com>
2020-11-03 13:01:00 -0300

    Restore lock. Add logs.

--- middlewares/download.js
+++ middlewares/download.js
@@ -1,15 +1,15 @@
 import fs from 'fs';
 
-// import Redis from 'ioredis';
-// import Redlock from 'redlock';
+import Redis from 'ioredis';
+import Redlock from 'redlock';
 import AWS from 'aws-sdk';
 
 import { flush } from './cache';
 
-// const redis = new Redis({ host: process.env.REDIS_HOST });
-// const redlock = new Redlock([redis], {
-//   retryCount: -1
-// });
+const redis = new Redis({ host: process.env.REDIS_HOST });
+const redlock = new Redlock([redis], {
+  retryCount: -1
+});
 
 const s3 = new AWS.S3({
   maxRetries: 10
@@ -23,31 +23,42 @@ const downloadFile = (req, res, next) => {
   const dirPath = `${process.env.CACHE_PATH}/${dir}/${region}/${bucket}`;
   const path = `${dirPath}/${filename}`;
 
+  console.log('Enter download', path);
+
   const download = async () => {
     // If file already exists, set path and call next middleware
     if (fs.existsSync(path)) {
+      console.log('Exists', path);
       res.locals.path = path;
       return next();
     }
 
+    console.log('Getting Lock', path);
+
     // Get the lock to avoid multiple downloads for the same file
-    // const lock = await redlock.lock(filename, 10000);
+    const lock = await redlock.lock(filename, 10000);
+
+    console.log('Lock', path, lock);
 
     // If something fails, unlock the queue and respond with error
     const fail = () => {
-      // lock.unlock().catch(next);
+      lock.unlock().catch(next);
+      console.log('Fail', path, lock);
       res.status(404).send('Error downloading source data file, please check params');
     };
 
     // Before calling download recursively, we have to unlock the queue
     // If the unlock fails is because the lock was lost so we can ignore the error
     const retry = () => {
-      // lock.unlock().catch((e) => {});
+      lock.unlock().catch((e) => {});
+      console.log('Retry', path, lock);
       download();
     };
 
     // Download error handler
     const onError = (error) => {
+      console.log('Error', path, error.code, lock);
+
       // If file not found, trigger error!
       if (error.code === 'NoSuchKey') {
         return fail();
@@ -65,12 +76,16 @@ const downloadFile = (req, res, next) => {
     // This is about milliseconds and it does not happen often
     // but sometimes the file gets downloaded just after the function gets the lock
     if (fs.existsSync(path)) {
+      console.log('Exists after lock', path, lock);
       return retry();
     }
+
+    console.log('Downloading', path, lock);
 
     // Download file from S3 to local cache
     s3.getObject({ Bucket: bucket, Key: key }).promise()
       .then(data => {
+        console.log('Downloaded', path, lock);
         fs.writeFileSync(path, data.Body, onError);
         setTimeout(retry, 1000);
       })
