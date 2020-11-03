9aacca43eac3d7923d663c724278e735112d09ed
Mauricio Miranda <maurimiranda@gmail.com>
2020-11-03 17:14:13 -0300

    Fixing bug

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,9 +3,9 @@ module.exports = {
     name: 'tiler',
     script: './server.js',
     node_args: '--experimental-modules --es-module-specifier-resolution=node',
-    instances: 6,
+    instances: 3,
     exec_mode: 'cluster',
-    max_memory_restart: '550M',
+    max_memory_restart: '2000M',
     watch: true,
     watch_delay: 1000,
     env: {
