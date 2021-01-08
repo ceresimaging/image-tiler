c0231faded6b73585aaa09615788c96a88f9bd73
Mauricio Miranda <maurimiranda@gmail.com>
2021-01-08 12:45:06 -0300

    Update tree tests

--- test/tree.test.js
+++ test/tree.test.js
@@ -4,7 +4,7 @@ const base = 'tree';
 
 describe('tree routes', () => {
   test('should return a vector tile with trees', async () => {
-    const overlay = '14d04294-6636-4fda-a82d-23dc1cd9e0e3';
+    const overlay = '4d5cca2a-5b18-4346-90af-8daa2bcbfc1e';
 
     const res = await request.get(`/${base}/count/${overlay}/15/5337/12656.mvt`).responseType('arraybuffer');
 
@@ -12,7 +12,7 @@ describe('tree routes', () => {
   });
 
   test('should return a vector tile with tree data', async () => {
-    const overlay = '8bcf2e4b-a18b-4857-90c2-8ee80fd8df98';
+    const overlay = '98e1c50d-1d6e-40ac-b955-2c8ab5df07cb';
 
     const res = await request.get(`/${base}/data/${overlay}/15/5337/12656.mvt`).responseType('arraybuffer');
 
