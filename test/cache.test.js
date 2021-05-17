7eaed8bb1dc321238adaf708ae59dd8bbe61ab0c
Mauricio Miranda <maurimiranda@gmail.com>
2021-05-17 17:01:26 -0300

    Remove local cache and everything related with it

--- test/cache.test.js
+++ test/cache.test.js
@@ -1,4 +1,4 @@
-import { request, createImagery, createCustom, createFiles, wipeCache } from "./helpers";
+import { request } from "./helpers";
 
 const base = "cache";
 const imagery = "7326e81d-40b0-4053-8f33-cd22f9a53df9";
@@ -7,29 +7,19 @@ const custom = "0e220754-e251-41c2-ab8b-1f05962ab7e9";
 jest.mock("aws-sdk");
 
 describe("cache", () => {
-  beforeEach(() => {
-    wipeCache();
-  });
-
   test("should remove an imagery cached file", async () => {
-    createImagery(imagery);
-
     const res = await request.get(`/${base}/imagery/${imagery}`);
 
     expect(res.body).toEqual({ invalidations: 4 });
   });
 
   test("should remove an imagery cached file and wait for invalidation", async () => {
-    createImagery(imagery);
-
     const res = await request.get(`/${base}/imagery/${imagery}?wait=true`);
 
     expect(res.body).toEqual({ invalidations: 4 });
   });
 
   test("should remove a custom layer cached file", async () => {
-    createCustom(custom);
-
     const res = await request.get(`/${base}/custom/${custom}`);
 
     expect(res.body).toEqual({ invalidations: 1 });
